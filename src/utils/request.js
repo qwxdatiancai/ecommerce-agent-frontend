637a3db4709196e6900aa4f1c05f50c6d039d598
qwxdatiancai <qiwenxuan055@gmail.com>
2026-04-28 18:49:05 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,9 +5,8 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  baseURL: 'https://qwx666-ecommerce-agent-api.hf.space', // 强行写死！
+  timeout: 5000
 })
 
 // request interceptor
